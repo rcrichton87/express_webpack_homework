c3aa4ecf718f06b851432cff807537780bdb9a6f
Ross Crichton <rcrichton87@googlemail.com>
2017-04-17 15:45:36 +0100

    can add a film

--- client/src/controllers/films.js
+++ client/src/controllers/films.js
@@ -1,4 +1,5 @@
 var Films = require('../models/films');
+var Film = require('../models/film');
 
 var films = new Films();
 var express = require('express');
@@ -6,16 +7,21 @@ var filmRouter = new express.Router();
 
 filmRouter.get('/', function(req, res){ 
   res.json(films);
-})
+});
 
 filmRouter.get('/:id', function(req, res){ 
   res.json(films[req.params.id]);
-})
+});
 
 filmRouter.put('/:id', function(req, res){
   films[req.params.id].title = req.body.title;
   films[req.params.id].actors = req.body.actors; 
   res.json(films);
-})
+});
+
+filmRouter.post('/', function(req, res){
+  films.push(new Film(req.body) );
+  res.json(films);
+});
 
 module.exports = filmRouter;
